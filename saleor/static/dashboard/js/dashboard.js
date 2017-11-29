79c87346d7777932a092628c7a7714c3f41cf39e
dominik-zeglen <flesz3@o2.pl>
2017-11-29 14:30:07 +0100

    Change dashboard's JS structure

--- saleor/static/dashboard/js/dashboard.js
+++ saleor/static/dashboard/js/dashboard.js
@@ -1,15 +1,4 @@
 import 'materialize-css/dist/js/materialize';
 
 import '../scss/dashboard.scss';
-
-import './components/async-form';
-import './components/bulk-actions';
-import './components/coupons';
-import './components/datepicker';
-import './components/modal';
-import './components/image-gallery';
-import './components/misc';
-import './components/filters';
-import './components/side-menu';
-import './components/styleguide';
-import './components/tab';
+import './components';
